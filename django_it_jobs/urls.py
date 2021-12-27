3c5e6fa50f2b0d08281951252aeef3918909ff0d
Bartosz <radu480@o2.pl>
2021-12-27 18:25:45 +0100

    Modify urls for jobs once again.

--- django_it_jobs/urls.py
+++ django_it_jobs/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('jobs.urls')),
+    path('api/', include('jobs.urls')),
 ]
