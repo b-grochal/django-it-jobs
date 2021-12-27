3a3001956ee9a3c7603fd2f32bade6b0e3d03b56
Bartosz <radu480@o2.pl>
2021-12-27 17:23:54 +0100

    Adjust admin panel for jobs.

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Job, Application
+from django.contrib import admin
 
-# Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'salary', 'work_type', 'company_name', 'city', 'country')
+    list_filter = ('name', 'company_name', 'city', 'country')
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.site_header = "IT Jobs Admin Panel"
